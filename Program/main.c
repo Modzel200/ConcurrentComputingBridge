e3643721ca9f91cfce7b43e7c00b58823feb3e93
Modzel200 <modzelewskijakub20@gmail.com>
2023-05-17 18:24:57 +0200

    struct

--- Program/main.c
+++ Program/main.c
@@ -1,5 +1,48 @@
 #include <stdio.h>
-int main() {
-   printf("Hello, World!");
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <struct.c>
+#include <time.h>
+pthread_mutex_t mutex;
+int variable=0;
+void* test()
+{
+   for(int i=0;i<1000000;i++)
+   {
+      pthread_mutex_lock(&mutex);
+      variable++;
+      pthread_mutex_unlock(&mutex);
+
+   }
+}
+
+int main(int argc, char *argv[]) {
+   if(argc<2)
+   {
+      printf("Nie podano parametru okreslajacego liczbe samochodow\n");
+      return 0;
+   }
+   pthread_mutex_init(&mutex,NULL);
+   printf("Liczba pojazdow: %s\n",argv[1]);
+   int numberOfThreads = atoi(argv[1]);
+   pthread_t listOfThreads[numberOfThreads];
+   for(int i=0;i<numberOfThreads;i++)
+   {
+      if(pthread_create(&listOfThreads[i],NULL,&test,NULL)!=0)
+      {
+         return 0;
+      }      
+   }
+   for(int i=0;i<numberOfThreads;i++)
+   {
+      if(pthread_join(listOfThreads[i],NULL)!=0)
+      {
+         return 0;
+      }      
+   }
+   pthread_mutex_destroy(&mutex);
+   printf("Ilość wykonanych iteracji: %d\n",variable);
    return 0;
 }
+
---
+++ Program/struct.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+typedef struct Car {
+    int carId;
+    long int threadId;
+    bool isWaiting;
+    char cityName[32];
+    int idleMeter;
+    struct Car * next;
+} Car_t;
+
+void addCar(Car_t **carStart, int carId, int threadId, const char* cityName, int idleMeter){
+    if(*carStart ==NULL){
+        *carStart = (Car_t*)malloc(sizeof(Car_t));
+        (*carStart)->carId = carId;
+        (*carStart)->threadId = threadId;
+        (*carStart)->isWaiting = false;
+        strcpy((*carStart)->cityName,cityName);
+        (*carStart)->idleMeter = idleMeter;
+        (*carStart)->next = NULL;
+    } else {
+        Car_t *currentCar = *carStart;
+        while(currentCar->next!=NULL){
+            currentCar=currentCar->next;
+        }
+        currentCar->next = (Car_t *)malloc(sizeof(Car_t));
+        currentCar->carId = carId;
+        currentCar->threadId = threadId;
+        currentCar->isWaiting = false;
+        strcpy(currentCar->next->cityName,cityName);
+        currentCar->next->idleMeter=idleMeter;
+        currentCar->next->next=NULL;
+    }
+    return;
+}
