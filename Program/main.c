f9f705adffaa1aa0141ab8028da5747ac07ad70d
c0ndii <105825543+c0ndii@users.noreply.github.com>
2023-05-25 15:06:52 +0200

    fajrant

--- Program/main.c
+++ Program/main.c
@@ -6,51 +6,48 @@
 #include <time.h>
 #include "simulation.c"
 #include "random.c"
+#include "simulationEnvironmentVariables.c"
 
 int variable=0;
 
 int main(int argc, char *argv[]) {
-   srand(time(NULL));
-   if(argc<2)
-   {
-      printf("Nie podano parametru okreslajacego liczbe samochodow\n");
-      return 0;
-   }
-   //printf("Randomowa liczba%d",randValue());
-   //printf("Liczba pojazdow: %s\n",argv[1]);
-   int numberOfThreads = atoi(argv[1]);
-   
-   Car_t *cars = NULL;
-   char *places[3];
-   places[0]="CityA";
-   places[1]="CityB";
-   places[2]="Bridge";
-   char *place;
-   for(int i=0;i<numberOfThreads;i++)
-   {
-      if(rand()%2==0)
-      {
-         place = places[0];
-      }
-      else
-      {
-         place = places[1];
-      }
-      addCar(&cars,i,0,place,rand()%5+1);
-   }
-   
-   /*
-   for(int i=0;i<numberOfThreads;i++)
-   {
-      if(pthread_join(listOfThreads[i],NULL)!=0)
-      {
-         return 0;
-      }      
-   }
-   */
-   //printAllCars(cars);
-   simulation(&cars,numberOfThreads);
-   printf("Ilość wykonanych iteracji: %d\n",variable);
-   return 0;
+    srand(time(NULL));
+    if(argc<2)
+    {
+        printf("Nie podano parametru okreslajacego liczbe samochodow\n");
+        return 1;
+    }
+    if(argc<3){
+        printf("Nie podano trybu dzialania programu\n");
+        return 1;
+    }
+    //printf("Randomowa liczba%d",randValue());
+    //printf("Liczba pojazdow: %s\n",argv[1]);
+    int numberOfThreads = atoi(argv[1]);
+    Car_t *cars = NULL;
+    char *places[3];
+    int workingMode = atoi(argv[2]);
+    places[0]="CityA";
+    places[1]="CityB";
+    places[2]="Bridge";
+    char *place;
+    for(int i=0;i<numberOfThreads;i++)
+    {
+        if(rand()%2==0)
+        {
+            place = places[0];
+        }
+        else
+        {
+            place = places[1];
+        }
+        addCar(&cars,i,0,place,rand()%5+1);
+    }
+    if(workingMode==0){
+        simulation(&cars,numberOfThreads);
+    } else {
+        simulationEnvironmentVariables(&cars, numberOfThreads);
+    }
+    return 0;
 }
 
---
+++ Program/simulationEnvironmentVariables.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <time.h>
+#include<unistd.h>
+pthread_cond_t envVar = PTHREAD_COND_INITIALIZER;
+pthread_mutex_t mutex;
+void *onBridgeEnv(void *car)
+{
+    Car_t *currentCar = (struct Car*)car;
+    pthread_cond_wait(&envVar,&mutex);
+    //printf("Jestem na moście moje id to: %ld\n",selfThread);
+    strcpy(currentCar->cityName,"Bridge");
+    sleep(2);
+    pthread_cond_signal(&envVar);
+}
+
+void simulationEnvironmentVariables(Car_t **cars, int numberOfThreads)
+{
+    pthread_mutex_init(&mutex,NULL);
+    Car_t *currentCar;
+    pthread_t *listOfThreads;
+    listOfThreads = calloc(numberOfThreads,sizeof(pthread_t));
+    currentCar=*cars;
+    int i;
+    char *lastCity;
+
+    while(1)
+    {
+        i=0;
+        //printAllCars(*cars);
+        currentCar=*cars;
+        while(currentCar!=NULL){
+            if(currentCar->idleMeter==0)
+            {
+                currentCar->isWaiting=true;
+            }
+            else
+            {
+                currentCar->idleMeter=currentCar->idleMeter-1;
+            }
+            if(currentCar->isWaiting==true)
+            {
+                draw(*cars,lastCity);
+                //pthread_mutex_lock(&mutex); // Blokuj mutex przed utworzeniem wątku
+                int tmpLen = strlen(currentCar->cityName);
+                lastCity = (char*)malloc((tmpLen + 1) * sizeof(char));
+                strcpy(lastCity, currentCar->cityName);
+                //pthread_mutex_unlock(&mutex); // Odblokuj mutex po utworzeniu wątku
+                pthread_create(&listOfThreads[i], NULL, onBridge, (void*)currentCar);
+                pthread_join(listOfThreads[i], NULL);
+                //pthread_mutex_lock(&mutex); // Blokuj mutex przed aktualizacją danych
+                currentCar->threadId=listOfThreads[i];
+                currentCar->idleMeter = rand() % 5 + 1;
+                currentCar->isWaiting = false;
+                //pthread_mutex_unlock(&mutex); // Odblokuj mutex po aktualizacji danych
+            }
+
+            if(strcmp(currentCar->cityName,"Bridge")==0)
+            {
+                draw(*cars,lastCity);
+                if(strcmp(lastCity,"CityA")==0)
+                {
+                    strcpy(currentCar->cityName,"CityB");
+                }
+                else
+                {
+                    strcpy(currentCar->cityName,"CityA");
+                }
+                free(lastCity);
+            }
+            currentCar = currentCar->next;
+            i++;
+        }
+        sleep(2);
+    }
+    pthread_mutex_destroy(&mutex);
+
+}
